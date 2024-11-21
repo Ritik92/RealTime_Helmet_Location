99d3b22ad62d8b04b23e33317783f2faff6fb587
Ritik92 <kartik292004@gmail.com>
2024-11-21 12:55:08 +0530

    ef

--- backend/index.js
+++ backend/index.js
@@ -27,7 +27,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
     cors({
-        origin: "http://localhost:3001",
+        origin: "https://mindshield.ritikcrafts.org",
         credentials: true,
     })
 );
